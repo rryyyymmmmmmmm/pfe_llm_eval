5f9fec4bbf9bb8a349f32d7359ab997b720670ed
rym <87146313+rryyyymmmmmmmm@users.noreply.github.com>
2026-05-07 19:10:55 +0100

    added new deepeval evaluation and all examples, haven't tested it yet

---
+++ verification.py
@@ -0,0 +1,14 @@
+# For the verification, after having outputted the structured json from the LLM, we need to : 
+# 1- make sure that stamps exist (hadi abderahim darha) 
+# 2- make sure the vignettes match the medicaments
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
